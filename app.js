fc1aa00a5e28504fd9dd7d29b2388e2e2a98b2d7
ahsankalim125-hub <ahsankalim215@gmail.com>
2026-06-30 22:14:32 +0500

    J.S-04-Assignment

---
+++ app.js
@@ -0,0 +1,18 @@
+let percentage = Number(prompt("Sharmaomat"));
+
+
+if(percentage < 0 || percentage > 100){
+    alert("Invalid Percentage");
+}else if(percentage>=90){
+    alert("A+");
+}else if(percentage>=80){
+    alert("A");
+}else if(percentage>=70){
+    alert("B");
+}else if(percentage>=60){
+    alert("C");
+}else if(percentage>=50){
+    alert("D");
+}else {
+    alert("Fail");
+}
